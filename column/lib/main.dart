559a36d7a67104b82027b5003615f323ddab08a4
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-08-16 13:13:45 +0500

    odamcha

--- column/lib/main.dart
+++ column/lib/main.dart
@@ -1,115 +1,303 @@
+
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() {
-  runApp(const MyApp());
+// void main(){
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Text('\nHello world',
+//     textAlign: TextAlign.center,
+//     style: TextStyle(fontSize: 40,color: Colors.green),
+//     textDirection: TextDirection.ltr,),
+//   ));
+// }
+
+
+//10
+// void main(){
+//   runApp(MaterialApp(
+// 
+//     debugShowCheckedModeBanner: false,
+//     home: Text('\nHello world',
+//     textAlign: TextAlign.center,
+//     style: TextStyle(fontSize: 40,color: Colors.green),
+//     textDirection: TextDirection.ltr,),
+//   ));
+// }
+
+
+
+
+//11
+// void main(){
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Row(children: [Container(
+//           margin: const EdgeInsets.all(5),
+//           width: 102,
+//           height: 102,
+//           color: Colors.white,),
+//           Container(
+//             margin: const EdgeInsets.all(5),
+//             width: 102,
+//           height: 102,
+//           color: Colors.lightBlue
+//           ),
+//           Container(
+//             margin: const EdgeInsets.all(5),
+//             width: 102,
+//           height: 102,
+//           color: Colors.red
+//           )
+//         ],
+//     ),
+//     Row(children: [Container(
+//           margin: const EdgeInsets.all(5),
+//           width: 102,
+//           height: 102,
+//           color: Colors.white,),
+//           Container(
+//             margin: const EdgeInsets.all(5),
+//             width: 102,
+//           height: 102,
+//           color: Colors.lightBlue
+//           ),
+//           Container(
+//             margin: const EdgeInsets.all(5),
+//             width: 102,
+//           height: 102,
+//           color: Colors.yellow
+//           )
+//         ],
+//     ),
+//     Row(children: [Container(
+//           margin: const EdgeInsets.all(5),
+//           width: 102,
+//           height: 102,
+//           color: Colors.red,),
+//           Container(
+//             margin: const EdgeInsets.all(5),
+//             width: 102,
+//           height: 102,
+//           color: Colors.yellow
+//           ),
+//           Container(
+//             margin: const EdgeInsets.all(5),
+//             width: 102,
+//           height: 102,
+//           color: Colors.blue
+//           )
+//         ],),],
+//     )
+//    )
+//   );
+// }
+
+
+// Widget getBox({double size=150,Color color=Colors.red}){
+//   return Container(
+//     color: color,
+//     width: size,
+//     height: size,
+//     margin: const EdgeInsets.all(5),
+//   );
+// }
+
+// void main(){
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [Container(
+//       color: Colors.white,),
+//       Row(children: [getBox(),getBox()],),
+//       Row(children: [getBox(size: 200)],),
+//     ]),)
+//   );
+// }
+
+
+
+//1111111111
+// Widget func({double size=100,double siz=100,Color color=Colors.lightBlue}){
+//   return Container(
+//     padding: const EdgeInsets.all(10),
+//    child: Container(color:  Color.fromARGB(255, 224, 215, 215),
+//     padding: const EdgeInsets.all(10),
+//     child: Container(color:  Color.fromARGB(255, 13, 124, 228),
+//     margin: const EdgeInsets.all(10),
+//     width: size,
+//     height: siz,
+//   )
+//   ));
+// }
+// void main(){
+//   Color x=Colors.blue;
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Container(
+//     color: Colors.white,
+//     child: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [func(),func()]),
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [func(size: 260)]),
+
+//     ],
+//     ),)));
+// }
+
+
+
+
+// void main(){
+//   Color x=Colors.white;
+//   runApp(Container(color: x,));
+// }
+
+//2222222
+// Widget Salohiddin({double size=10,double siz=100,Color color=Colors.red}){
+
+// return Container(
+//   padding: const EdgeInsets.all(10),
+//   child: Container(color:  Color.fromARGB(255, 207, 202, 202),
+//   padding: const EdgeInsets.all(10),
+//   child: Container(color: Colors.blue),
+//   width: size,
+//   height: siz,
+//   margin: const EdgeInsets.all(5),
+// ));
+// }
+// void main(){
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Container(
+//       color: Colors.white,
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(),Salohiddin()]),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(size: 330)]),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(),Salohiddin()]),
+
+//         ]),
+//      ),
+//   ));
+// }
+
+
+
+//3333333
+
+
+// Widget Salohiddin({double size=55,double siz=70,Color color=Colors.red}){
+
+// return Container(
+//   padding: const EdgeInsets.all(5),
+//   child: Container(color:  Color.fromARGB(255, 207, 202, 202),
+//   padding: const EdgeInsets.all(8),
+//   child: Container(color: Colors.blue),
+//   width: size,
+//   height: siz,
+//   margin: const EdgeInsets.all(2),
+// ));
+// }
+// void main(){
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Container(
+//       color: Colors.white,
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(size: 55,siz: 70)]),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(),Salohiddin()]),
+//             Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(siz: 70,size: 55),Salohiddin(siz: 70,size: 55),Salohiddin(siz: 70,size: 55),]),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(siz: 70,size: 55),Salohiddin(siz: 70,size: 55),
+//             Salohiddin(siz: 70,size: 55),Salohiddin(siz: 70,size: 55)]),
+//             Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(siz: 70,size: 55),Salohiddin(siz: 70,size: 55),Salohiddin(siz: 70,size: 55),]),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(),Salohiddin()]),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children:   [Salohiddin(size: 55,siz: 70)]),
+
+//         ]),
+//      ),
+//   ));
+// }
+
+
+
+
+//44444
+
+
+Widget Salohiddin({double size=55,double siz=70,Color color=Colors.red}){
+
+return Container(
+  padding: const EdgeInsets.all(5),
+  child: Container(color:  Color.fromARGB(255, 207, 202, 202),
+  padding: const EdgeInsets.all(8),
+  child: Container(color: Colors.blue),
+  width: size,
+  height: siz,
+  margin: const EdgeInsets.all(2),
+));
 }
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [Salohiddin(size: 150,siz: 50)]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [Salohiddin(size:100,siz: 100 )]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [Salohiddin(size: 280,siz: 40)]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [Salohiddin(siz: 200,size: 50),Salohiddin(siz: 200,size: 150),
+            Salohiddin(siz: 200,size: 50)]),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [Salohiddin(siz: 200,size: 70),Salohiddin(siz: 200,size: 70)]),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [Salohiddin(siz: 0,size: 20)]),
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        ]),
+     ),
+  ));
 }
